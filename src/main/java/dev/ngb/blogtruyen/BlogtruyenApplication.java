d49c2bf5e81898e97e5509f1bf502e9ab8362774
duykhanh <ninggiangboy@gmail.com>
2024-01-23 23:36:19 +0700

    init project

---
+++ src/main/java/dev/ngb/blogtruyen/BlogtruyenApplication.java
@@ -0,0 +1,13 @@
+package dev.ngb.blogtruyen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogtruyenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogtruyenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ngb/blogtruyen/BlogtruyenApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ngb.blogtruyen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogtruyenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
